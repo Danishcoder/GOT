eb8e49c14d6adc359ac80dc94d2fd70e27ca70b2
DanishCoder <Danishim66@gmail.com>
2022-05-25 14:36:33 -0400

    Added jon snow defense

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let jonSnowAttack = 25;
 let jonSnowHealth = 100;
 let jonSnowDefense = 0;
 // Jamie Stats 
-let jamieLannisterAttack = 25;
+let jamieLannisterAttack = 35;
 
 if (jonSnowAttack > jamieLannisterAttack) {
     console.log("Victory for Jon Snow!!")
@@ -19,5 +19,14 @@ if (jonSnowHealth <= jamieLannisterAttack) {
         //jonSnowHealth = jonSnowHealth - jamieLannisterAttack
       jonSnowHealth -= jamieLannisterAttack
         console.log(`Jon Snow's Health is down to ${jonSnowHealth}`)
-        
+}
+// Jon pick's up a shield
+jonSnowDefense += 25;
+
+if (jonSnowHealth is <= jamieLannisterAttack - jonSnowDefense) {
+    console.log("Jon Snow is dead")
+else {
+    health -= (jamieLannisterAttack - jonSnowDefense)
+    console.log(`Jon Snow's Health is down to ${jonSnowHealth}`)
+}
 }
