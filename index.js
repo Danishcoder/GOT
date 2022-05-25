591d1cef4d6775103f9a81a327490796c594a253
DanishCoder <Danishim66@gmail.com>
2022-05-25 14:16:35 -0400

    Initial commit sizing up

---
+++ index.js
@@ -0,0 +1,8 @@
+let jonSnowAttack = 25;
+let jamieLannisterAttack = 35;
+
+if (jonSnowAttack > jamieLannisterAttack){
+    console.log("Victory for Jon Snow!!")
+} else {
+    console.log("Victory for Jamie Lannister!")
+}
